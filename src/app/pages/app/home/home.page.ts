d72ff8c6a15e472066bebff0237f8e5b5e4a7969
Ethan Borkosky <ethan.borkosky@gmail.com>
2021-05-27 00:02:17 -0400

    spotify auth

--- src/app/pages/app/home/home.page.ts
+++ src/app/pages/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  public access_token: string;
+
+  constructor(
+    public router: Router,
+  ) { 
+    let hash = window.location.hash;
+    // Lazy, revise
+    this.access_token = hash.substr(14, 204);
+  }
 
   ngOnInit() {
+    
+  }
+
+  async authorizeSpotifyUser() {
+
+    // await fetch('https://accounts.spotify.com/authorize' + new URLSearchParams({
+    //   client_id: '4c028c1ba1344499b4cf174f5ce4363b',
+    //   response_type: 'token',
+    //   redirect_uri: 'http://localhost:8100/app/home'
+    // }))
+    // let url = 'https://accounts.spotify.com/authorize'
+    window.location.href = 'https://accounts.spotify.com/authorize?' + new URLSearchParams({
+      client_id: '4c028c1ba1344499b4cf174f5ce4363b',
+      response_type: 'token',
+      redirect_uri: 'http://localhost:8100/app/home',
+      scope: 'playlist-modify-public playlist-modify-private'
+    });
+    
   }
 
 }
